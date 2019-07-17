bb27fce30a3293a751b2da2a3099a55d7527c4c8
phil webb <philip.webb@iag.com.au>
2019-07-17 15:52:28 +1000

    mark59 core and selenium
    mark59 core and selenium  - prep for maven central

---
+++ mark59-selenium-implementation/src/main/java/com/mark59/selenium/corejmeterimpl/SeleniumAbstractJavaSamplerClient.java
@@ -0,0 +1,308 @@
+/*
+ *  Copyright 2019 Insurance Australia Group Limited
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mark59.selenium.corejmeterimpl;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+
+import com.mark59.core.utils.AppConstants;
+import com.mark59.core.utils.IpUtilities;
+import com.mark59.core.utils.Log4jConfigurationHelper;
+import com.mark59.core.utils.Mark59Utils;
+import com.mark59.selenium.drivers.SeleniumDriverFactory;
+import com.mark59.selenium.drivers.SeleniumDriverWrapper;
+import com.mark59.selenium.utils.SafeSleep;
+
+
+/**
+ * Selenium flavoured extension of the Jmeter Java Sampler AbstractJavaSamplerClient.
+ * 
+ * <p>A core class of the mark59 Selenium implementation, and should be extended when  
+ * creating Jmeter-ready selenium script. 
+ * 
+ * <p>Implementation of abstract method runSeleniumTest should contain the test, with
+ * parameterisation handled by additionalTestParameters.  See the 'DataHunter' sample provided for
+ * implementation details 
+ *      
+ * <p>Includes a number of standard parameters expected for a Selenium WebDriver.</p>
+ *
+ * @author Philip Webb
+ * Written: Australian Winter 2019  
+ */
+public abstract class SeleniumAbstractJavaSamplerClient  extends AbstractJavaSamplerClient {
+
+	public static Logger LOG = Logger.getLogger(SeleniumAbstractJavaSamplerClient.class);	
+	
+	public static final String LOG_SCREENSHOTS_AT_START_OF_TRANSACTIONS = "Log_Screenshots_At_Start_Of_Transactions";	
+	
+	protected Arguments jmeterArguments = new Arguments();
+
+	protected JmeterFunctionsForSeleniumScripts jm;	
+	protected SeleniumDriverWrapper seleniumDriverWrapper; 
+	protected WebDriver driver;
+
+	private KeepBrowserOpen keepBrowserOpen = KeepBrowserOpen.NEVER;
+
+	
+	private static final Map<String,String> defaultArgumentsMap; 	
+	static {
+		Map<String,String> staticMap = new LinkedHashMap<String,String>();
+		
+		staticMap.put("______________________ driver settings: ________________________", "Refer Mark59 User Guide: http://pv-central.auiag.corp:81/mark59guides/Mark59_User_Guide.html" );	
+		staticMap.put(SeleniumDriverFactory.DRIVER,						AppConstants.DEFAULT_JMETER_MODE);
+		staticMap.put(SeleniumDriverFactory.HEADLESS_MODE, 				String.valueOf(true));
+		staticMap.put(SeleniumDriverFactory.PAGE_LOAD_STRATEGY, 		PageLoadStrategy.NORMAL.toString());
+		staticMap.put(SeleniumDriverFactory.PROXY, 						"");
+		staticMap.put(SeleniumDriverFactory.ADDITIONAL_OPTIONS, 		"");				
+		staticMap.put(SeleniumDriverFactory.WRITE_FFOX_BROWSER_LOGFILE, String.valueOf(false));		
+		
+		staticMap.put("______________________ logging settings: _______________________", "Expected values: 'default', 'buffer', 'write' or 'off' ");		
+		staticMap.put(SeleniumDriverWrapper.LOG_SCREENSHOTS_AT_START_OF_TRANSACTIONS,	SeleniumDriverWrapper.DEFAULT);
+		staticMap.put(SeleniumDriverWrapper.LOG_SCREENSHOTS_AT_END_OF_TRANSACTIONS, 	SeleniumDriverWrapper.DEFAULT);
+		staticMap.put(SeleniumDriverWrapper.LOG_PAGE_SOURCE_AT_START_OF_TRANSACTIONS,	SeleniumDriverWrapper.DEFAULT);
+		staticMap.put(SeleniumDriverWrapper.LOG_PAGE_SOURCE_AT_END_OF_TRANSACTIONS, 	SeleniumDriverWrapper.DEFAULT);
+		staticMap.put(SeleniumDriverWrapper.LOG_PERF_LOG_AT_END_OF_TRANSACTIONS, 		SeleniumDriverWrapper.DEFAULT);
+		
+		staticMap.put("______________________ miscellaneous: __________________________", "");				
+		staticMap.put(IpUtilities.RESTRICT_TO_ONLY_RUN_ON_IPS_LIST, "");			
+		
+		defaultArgumentsMap = Collections.unmodifiableMap(staticMap);
+	}
+	
+	
+	
+	
+	/** 
+	 * Creates the list of parameters with default values, as they would appear on the Jmeter GUI for the JavaSampler being implemented.
+	 * <p>A standard set of parameters are defined (defaultArgumentsMap). Additionally,an implementing class (the script extending this class) 
+	 * can add additional parameters (or override the standard defaults) via the additionalTestParameters() method.    
+	 * 
+	 * @see #additionalTestParameters()
+	 * @see org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient
+	 */
+	@Override
+	public Arguments getDefaultParameters() {
+		return Mark59Utils.mergeMapWithAnOverrideMap(defaultArgumentsMap, additionalTestParameters());
+	}
+
+
+	@Override
+	public void setupTest(JavaSamplerContext context) {
+		super.setupTest(context);
+	}
+
+
+	
+	/**
+	 * Used to define required parameters for the test and their default values.
+	 * <p>Ultimately used to build a Map of key-value pairs that will be available throughout the 'Mark59' framework
+	 * for whatever customization is required for your test or driver implementation.</p>
+	 * <p>Note: Can be used to override pre-defined parameters (the defaultArgumentsMap in SeleniumAbstractJavaSamplerClient)</p> 
+	 * <p>Refer to the 'see also' list (additionalTestParameters() in SeleniumAbstractJavaSamplerClient) detailing some key pre-defined parameters available.</p>  
+	 * 
+	 * @see SeleniumDriverFactory#getDriverBuilderOfType  
+	 * @see com.mark59.selenium.drivers.SeleniumDriverBuilder#setHeadless(boolean)  
+	 * @see com.mark59.selenium.drivers.SeleniumDriverBuilder#setPageLoadStrategy(PageLoadStrategy)  
+	 * @see com.mark59.selenium.drivers.SeleniumDriverBuilder#setProxy(org.openqa.selenium.Proxy) 
+	 * @see com.mark59.selenium.drivers.SeleniumDriverBuilder#setAdditionalOptions(java.util.List)  
+	 * @see com.mark59.selenium.drivers.SeleniumDriverBuilder#setWriteBrowserLogfile(boolean)
+	 * @see IpUtilities#localIPisNotOnListOfIPaddresses(String)   
+	 * @see JmeterFunctionsForSeleniumScripts
+	 * 
+	 * @return the updated list of jmeter arguments with any required changes
+	 */
+	protected abstract Map<String, String> additionalTestParameters();
+	
+	
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 *  Note the use of the catch on  AssertionError - as this is NOT an Exception but an Error, and therefore need to be explicitly caught. 
+	 */
+	@Override
+	public SampleResult runTest(JavaSamplerContext context) {
+		
+		if (IpUtilities.localIPisNotOnListOfIPaddresses(context.getParameter(IpUtilities.RESTRICT_TO_ONLY_RUN_ON_IPS_LIST))){ 
+			return null;
+		}
+	
+		if (LOG.isDebugEnabled()) LOG.debug(this.getClass().getName() +  " : exectuing runTest" );
+		
+		Map<String,String> jmeterRuntimeArgumentsMap = convertJmeterArgumentsToMap(context);
+		
+		seleniumDriverWrapper = new SeleniumDriverFactory().makeDriverWrapper(jmeterRuntimeArgumentsMap) ;
+
+		driver =  (WebDriver)seleniumDriverWrapper.getDriverPackage() ;
+		jm = new JmeterFunctionsForSeleniumScripts(Thread.currentThread().getName(), seleniumDriverWrapper, jmeterRuntimeArgumentsMap);   	
+		
+		try {
+			
+			LOG.debug(">> running test ");			
+			
+			runSeleniumTest(context, jm, driver);
+			
+			jm.tearDown();
+		
+			LOG.debug("<< finished test" );
+
+		}	catch (Exception | AssertionError e) {
+
+			System.err.println("ERROR : " + this.getClass() + ".  Exception " +  e.getClass().getName() +  " thrown.  See log and screenshot directory for details.  Stack trace:");
+			e.printStackTrace();
+			LOG.error("ERROR : " + this.getClass() + ".  Exception " +  e.getClass().getName() +  " thrown",  e);
+			
+			jm.failTest();
+			jm.tearDown();
+			
+			Exception x = new Exception(e);
+			seleniumDriverWrapper.documentExceptionState(x);
+			
+			if (keepBrowserOpen.equals(KeepBrowserOpen.ONFAILURE)){
+				// force browser to stay open
+				keepBrowserOpen = KeepBrowserOpen.ALWAYS;   
+			}
+			
+		} finally {
+			if (! keepBrowserOpen.equals(KeepBrowserOpen.ALWAYS )     ) { 
+				seleniumDriverWrapper.driverDispose();
+			}
+		}
+		return jm.getMainResult();
+	}
+
+	
+	
+	protected abstract void runSeleniumTest(JavaSamplerContext context, JmeterFunctionsForSeleniumScripts jm, WebDriver driver);
+
+	
+	
+	private Map<String,String> convertJmeterArgumentsToMap(JavaSamplerContext context) {
+		Map<String, String> jmeterArgumentsAsMap = new HashMap<>();
+		
+		for (Iterator<String> iterator = context.getParameterNamesIterator(); iterator.hasNext();) {
+			String paramKey = (String) iterator.next();
+			jmeterArgumentsAsMap.put(paramKey, context.getParameter(paramKey) );
+		}
+		
+		if (LOG.isDebugEnabled()) LOG.debug("context parameters at convert... :  "  + Arrays.toString(jmeterArgumentsAsMap.entrySet().toArray()));
+		return jmeterArgumentsAsMap;
+	}
+
+	
+	/**
+	 * Convenience method to a single-threaded script execution (rather than needing to use Jmeter).  See method runMultiThreadedSeleniumTest to executed a multi-thread test<p>
+	 * It is assumed you are executing for debug purposes, so the default is to keep the browser open at test end the test fails(unless running in headless mode). 
+	 * 
+	 * @see com.mark59.selenium.corejmeterimpl.KeepBrowserOpen
+	 * @see Log4jConfigurationHelper
+	 * @param keepBrowserOpen  see KeepBrowserOpen
+	 */
+	protected void runSeleniumTest(KeepBrowserOpen keepBrowserOpen ) {
+		mockJmeterProperties();
+		JavaSamplerContext context = new JavaSamplerContext( getDefaultParameters()  );
+		
+		this.keepBrowserOpen = keepBrowserOpen;
+		if (String.valueOf(true).equalsIgnoreCase(context.getParameter(SeleniumDriverFactory.HEADLESS_MODE))) {
+			this.keepBrowserOpen = KeepBrowserOpen.NEVER;
+		};
+		LOG.info("keepBrowserOpen is set to "+ this.keepBrowserOpen);
+		
+		setupTest(context);
+		runTest(context);
+	}
+
+
+	/**
+	 *  @see #runSeleniumTest(KeepBrowserOpen)
+	 */
+	protected void runSeleniumTest() {
+		runSeleniumTest(KeepBrowserOpen.ONFAILURE);
+	}
+
+	/**
+	 * Convenience method to directly execute multiple script threads (rather than needing to use Jmeter).  
+	 * @see #runSeleniumTest(KeepBrowserOpen)
+	 * 
+	 * @param numberOfThreads number Of Java Threads
+	 * @param threadStartGapMs  time between start of each thread
+	 */
+	protected void runMultiThreadedSeleniumTest(int numberOfThreads, int threadStartGapMs) {
+		mockJmeterProperties();
+		
+		for (int i = 1; i <= numberOfThreads; i++) {
+			
+			new Thread(new SeleniumTestThread(this.getClass()), String.format("%03d", i)).start();
+			SafeSleep.sleep(threadStartGapMs);
+		}
+	}
+
+	
+	/**
+	 * Sets some jmeter properties to default values, if a jmeter.properties file is provided.  Only meant for use 
+	 * during script testing (eg in an IDE).  Prevents log WARNings for non-existent properties.<p>
+	 * If jmeter properties become relevant to a particular script for some reason, it is suggested the required 
+	 * 'jmeter.properties' file be included in the root of the project
+	 */
+	protected void mockJmeterProperties() { 
+		File f = new File("./jmeter.properties");
+		if(f.exists() && !f.isDirectory()) { 
+			LOG.debug("loading supplied jmeter.properties file");
+			JMeterUtils.loadJMeterProperties("./jmeter.properties");   	
+		}
+	}
+	
+	
+	/**
+	 * Convenience inner class to enable multi-thread testing outside of Jmeter
+	 */
+	public class SeleniumTestThread implements Runnable {
+
+		private Class<? extends SeleniumAbstractJavaSamplerClient>  testClass;
+
+		public SeleniumTestThread(Class<? extends SeleniumAbstractJavaSamplerClient>  testClass) {
+			this.testClass = testClass;
+		}
+
+		public void run() {
+			SeleniumAbstractJavaSamplerClient testInstance = null;
+			try {
+				testInstance = testClass.newInstance();
+			} catch (Exception e) {	e.printStackTrace(); System.out.println(" Error " + e.getMessage()  ); } 
+			
+			JavaSamplerContext context = new JavaSamplerContext( getDefaultParameters()  );
+			
+			testInstance.setupTest(context);
+			testInstance.runTest(context);
+		}
+	}
+	
+}
---
+++ mark59-selenium-implementation/src/main/java/com/mark59/selenium/utils/SuperPageElement.java
@@ -0,0 +1,143 @@
+/*
+ *  Copyright 2019 Insurance Australia Group Limited
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mark59.selenium.utils;
+
+import java.time.Duration;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Useful class that implements a number of commonly used interactions with Selenium WebElement objects
+ *
+ * @author Michael Cohen
+ * @author Philip Webb
+ * Written: Australian Winter 2019  
+ */
+public abstract class SuperPageElement<T extends SuperPageElement<?>> {
+
+	private static final Logger LOG = Logger.getLogger(SuperPageElement.class);	
+	
+	protected WebDriver driver;
+	protected By by;
+	protected Duration timeout;
+	protected Duration pollingFrequency;
+	protected boolean waitCondionsDebugMode;
+
+	
+	public SuperPageElement(WebDriver driver, By by, Duration timeout, Duration pollingFrequency) {
+		this(driver, by, timeout, pollingFrequency, false); 
+	}
+
+	public SuperPageElement(WebDriver driver, By by, Duration timeout, Duration pollingFrequency, boolean waitCondionsDebugMode) {
+		this.driver = driver;
+		this.by = by;
+		this.timeout = timeout;
+		this.pollingFrequency = pollingFrequency;
+		this.waitCondionsDebugMode = waitCondionsDebugMode;
+	}
+
+	
+	public WebElement waitForAndFindElement() {
+		return waitUntilCondition(ExpectedConditions.elementToBeClickable(by));
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	public T click() {
+		waitForAndFindElement().click();
+		return (T) this;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T clear() {
+		waitForAndFindElement().clear();
+		return (T) this;
+	}
+	
+	public T waitUntillStale() {
+		return waitUntillStaleBy(by);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T waitUntillStaleBy(By otherBy) {
+		waitUntilCondition(ExpectedConditions.stalenessOf(driver.findElement(otherBy)));
+		return (T) this;
+	}
+	
+	/**
+	 * Waits until the condition 'this element is clickable' is met.
+	 * @return waitUntilClickableBy
+	 */
+	public T waitUntilClickable() {
+		return waitUntilClickableBy(by);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T waitUntilClickableBy(By otherBy) {
+		waitUntilCondition(ExpectedConditions.elementToBeClickable(otherBy));
+		return (T) this;
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public T waitUntilTextPresentInElement(By otherBy, String expectedText) {
+		waitUntilCondition(ExpectedConditions.textToBePresentInElementLocated(otherBy, expectedText));
+		return (T) this;
+	}
+
+	
+	public <C> C waitUntilCondition(ExpectedCondition<C> condition) {
+		
+		if (!waitCondionsDebugMode) {
+			return FluentWaitFactory.getFluentWait(driver, timeout, pollingFrequency).until(condition);
+		} else {
+			return runWaitUntilConditonsDebugMode(condition);  
+		}
+	}
+
+
+	
+	private <C> C runWaitUntilConditonsDebugMode(ExpectedCondition<C> condition) {
+		
+		int webdriverTimeOutInSeconds = new Long( pollingFrequency.getSeconds() ).intValue() + 1;          //will generally get set to 1 
+		
+		int numberOfAttempts = new Long( timeout.getSeconds() / webdriverTimeOutInSeconds ).intValue();  
+		
+		for (int i = 1; i <= numberOfAttempts; i++ ) {
+			try {
+				if (i>1) LOG.debug( "re-attempt (try #" + i + " of " + numberOfAttempts + ") timeout : " + webdriverTimeOutInSeconds + "s" );
+				return new WebDriverWait(driver, webdriverTimeOutInSeconds).until(condition);
+			} catch (Exception e) {
+				LOG.debug(" condition for " + condition.getClass() + " not met : " + e.getMessage() );
+			}
+		}  
+		
+		throw new RuntimeException("** Exhausted all attempts - forcing a failure. ");
+	}
+
+	
+	public By getBy() {
+		return by;
+	}
+
+}
