28b68ba277b33df094393169e9064e898c817684
phil webb <philip.webb@iag.com.au>
2019-07-17 16:46:45 +1000

    mark59 core and selenium

---
+++ mark59-core/src/main/java/com/mark59/core/utils/AppConstants.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2019 Insurance Australia Group Limited
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mark59.core.utils;
+
+/**
+ * @author Michael Cohen
+ * @author Philip Webb
+ * Written: Australian Winter 2019  
+ */
+public class AppConstants {
+
+	private AppConstants() {
+	}
+
+	public static final String QUOTE_OR_POLICY = "QUOTE_OR_POLICY";
+	public static final String QUOTE = "QUOTE";
+	public static final String POLICY = "POLICY";
+
+	public static final String ENDS_WITH = "ENDS_WITH";
+	public static final String CONTAINS = "CONTAINS";
+	
+	public static final String TRUE = "TRUE";
+	public static final String FALSE = "FALSE";
+	
+	public static final String DEFAULT_JMETER_MODE = "CHROME";
+	
+	public static final String DEFAULT_STANDALONE_MODE = "CHROME";
+	public static final String TESTCLASS = "TESTCLASS";
+	public static final String ENVIRONMENT = "ENVIRONMENT"; 
+
+}
